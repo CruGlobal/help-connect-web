da2a776801a08e8895f81296cf3ce6c9972bb6c4
Caleb Cox <canac@users.noreply.github.com>
2024-02-07 16:34:32 -0500

    Add TaskForm and Field components

---
+++ src/components/Field.tsx
@@ -0,0 +1,112 @@
+import Checkbox from "@mui/material/Checkbox";
+import FormControl from "@mui/material/FormControl";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormGroup from "@mui/material/FormGroup";
+import FormHelperText from "@mui/material/FormHelperText";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import { useField } from "formik";
+import { Field as FieldType } from "../types";
+
+interface FieldProps {
+  field: FieldType;
+}
+
+export const Field: React.FC<FieldProps> = ({ field }) => {
+  const [fieldProps, meta] = useField({
+    name: field.id.toString(),
+    validate: (value) =>
+      value || value === 0 ? undefined : "This field is required",
+  });
+  const helperText =
+    meta.error && meta.touched ? meta.error : field.description;
+  const hasError = !!(meta.touched && meta.error);
+
+  if (field.type === "checkbox") {
+    return (
+      <FormGroup>
+        <FormControlLabel
+          control={<Checkbox required={field.required} {...fieldProps} />}
+          label={field.label}
+        />
+        <FormHelperText>{field.description}</FormHelperText>
+      </FormGroup>
+    );
+  } else if (field.type === "datetime") {
+    return (
+      <TextField
+        label={field.label}
+        required={field.required}
+        helperText={helperText}
+        error={hasError}
+        {...fieldProps}
+      />
+    );
+  } else if (field.type === "file") {
+    return (
+      <TextField
+        type="file"
+        label={field.label}
+        required={field.required}
+        helperText={helperText}
+        error={hasError}
+        multiple={field.multiple}
+        inputProps={{
+          accept: field.mime_type,
+        }}
+        InputLabelProps={{
+          shrink: true,
+        }}
+        {...fieldProps}
+      />
+    );
+  } else if (field.type === "number") {
+    return (
+      <TextField
+        type="number"
+        label={field.label}
+        required={field.required}
+        helperText={helperText}
+        error={hasError}
+        {...fieldProps}
+      />
+    );
+  } else if (field.type === "select") {
+    return (
+      <FormControl required={field.required}>
+        <InputLabel>{field.label}</InputLabel>
+        <Select label={field.label} {...fieldProps}>
+          {field.options.map((option) => (
+            <MenuItem key={option} value={option}>
+              {option}
+            </MenuItem>
+          ))}
+        </Select>
+        <FormHelperText>{field.description}</FormHelperText>
+      </FormControl>
+    );
+  } else if (field.type === "static") {
+    return (
+      <Typography component="div">
+        {field.label}
+        {field.description && (
+          <Typography variant="body2">{field.description}</Typography>
+        )}
+      </Typography>
+    );
+  } else if (field.type === "string") {
+    return (
+      <TextField
+        label={field.label}
+        multiline={field.multiline}
+        required={field.required}
+        helperText={helperText}
+        error={hasError}
+        {...fieldProps}
+      />
+    );
+  }
+};

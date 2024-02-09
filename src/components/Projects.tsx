2a0a7add329998500a18e3a35bf3f78a228d2900
Daniel Bisgrove <daniel@bizz-websites.com>
2024-02-08 21:16:34 -0500

    Adding Project and Tasklist fields

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,63 +1,182 @@
-import {useField} from 'formik'
-import { projects as projectData } from '../mocks';
-import { Autocomplete, CircularProgress, TextField } from '@mui/material';
-import { ReactElement, useMemo, useState } from 'react';
+import { useField, Field } from "formik";
+import { projects as projectData } from "../mocks";
+import {
+  Autocomplete,
+  CircularProgress,
+  FormControl,
+  FormHelperText,
+  TextField,
+} from "@mui/material";
+import { ReactElement, useMemo, useState } from "react";
+import { TaskList } from "../types";
 
 export const Projects = () => {
-  const [projectField, projectMeta] = useField('projectId');
-  const [projects, setProjects] = useState(projectData)
+  const [projectField, projectMeta, projectHelpers] = useField("projectId");
+  const [tasktField, taskMeta, taskHelpers] = useField("taskId");
+  const [projects, setProjects] = useState(projectData || []);
+  const [taskList, setTaskList] = useState<TaskList[]>([]);
+  const [originalTaskList, setOriginalTaskList] = useState<TaskList[]>([]);
   const loading = false;
 
   const handleProjectSearch = (search: string) => {
-    console.log('handleProjectSearch', search)
     if (!search) {
-      setProjects(projectData)
-      return
+      setProjects(projectData);
+      return;
     }
-    const lowerCaseSearch = search.toLowerCase()
-    const filteredProjects = projectData.filter((project) => project.name.toLowerCase().includes(lowerCaseSearch))
-    setProjects(filteredProjects)
+    const lowerCaseSearch = search.toLowerCase();
+    const filteredProjects = projectData.filter((project) =>
+      project.name.toLowerCase().includes(lowerCaseSearch),
+    );
+    setProjects(filteredProjects);
+  };
+
+  const handleTaskListSearch = (search: string) => {
+    if (!search) {
+      setTaskList(originalTaskList);
+      return;
+    }
+    const lowerCaseSearch = search.toLowerCase();
+    const filteredTaskList = originalTaskList.filter((task) =>
+      task.label.toLowerCase().includes(lowerCaseSearch),
+    );
+    setTaskList(filteredTaskList);
+  };
+
+  function validate(value: string) {
+    let error;
+    if (!value) {
+      error = "This field is required";
+    }
+    return error;
   }
 
+  const handleProjectSelected = (newValue: number) => {
+    projectHelpers.setValue(newValue);
+    const project = projects.find((project) => project.id === newValue);
+    if (project) {
+      const tasks = [];
+      for (const [key, value] of Object.entries(project.task_list)) {
+        tasks.push({
+          id: key,
+          label: value,
+        });
+      }
+      setOriginalTaskList(tasks);
+      setTaskList(tasks);
+    }
+  };
+
+  const handleTaskListSelected = (newValue: string) => {
+    taskHelpers.setValue(newValue);
+  };
 
   const projectIds = useMemo(() => {
-    return projects.map((project) => project.id)
-  }, [projects])
+    return projects.map((project) => project.id) || [];
+  }, [projects]);
 
+  const tasksListIds = useMemo(() => {
+    return taskList.map((task) => task.id);
+  }, [taskList]);
 
   return (
-    
-    <Autocomplete
-      autoSelect
-      autoHighlight
-      loading={loading}
-      options={projectIds}
-      getOptionLabel={(projectId): string =>
-        projects.find((project) => project.id === projectId)?.name ?? ''
-      }
-      renderInput={(params): ReactElement => (
-        <TextField
-          {...params}
-          size={'medium'}
-          variant="outlined"
-          label={'Project'}
-          onChange={(e) => handleProjectSearch(e.target.value)}
-          InputProps={{
-            ...params.InputProps,
-            'aria-labelledby': 'Project',
-            endAdornment: (
-              <>
-                {loading && <CircularProgress color="primary" size={20} />}
-                {params.InputProps.endAdornment}
-              </>
-            ),
-          }}
-        />
+    <>
+      <Field name="projectId" validate={validate}>
+        {() => (
+          <FormControl
+            fullWidth={true}
+            error={!!projectMeta.error}
+            variant="standard"
+          >
+            <Autocomplete
+              autoSelect
+              autoHighlight
+              loading={loading}
+              options={projectIds}
+              getOptionLabel={(projectId): string =>
+                projects.find((project) => project.id === projectId)?.name ?? ""
+              }
+              renderInput={(params): ReactElement => (
+                <TextField
+                  {...params}
+                  variant="outlined"
+                  label={"Project"}
+                  onChange={(e) => {
+                    handleProjectSearch(e.target.value || "");
+                  }}
+                  InputProps={{
+                    ...params.InputProps,
+                    "aria-labelledby": "Project",
+                    endAdornment: (
+                      <>
+                        {loading && (
+                          <CircularProgress color="primary" size={20} />
+                        )}
+                        {params.InputProps.endAdornment}
+                      </>
+                    ),
+                  }}
+                />
+              )}
+              value={projectField.value}
+              onChange={(_, projectId) => handleProjectSelected(projectId)}
+              sx={{ mb: 2 }}
+            />
+            {projectMeta.error && (
+              <FormHelperText>{projectMeta.error}</FormHelperText>
+            )}
+          </FormControl>
+        )}
+      </Field>
+
+      {originalTaskList && (
+        <Field name="taskId" validate={validate}>
+          {() => (
+            <FormControl
+              fullWidth={true}
+              error={!!taskMeta.error}
+              variant="standard"
+            >
+              <Autocomplete
+                autoSelect
+                autoHighlight
+                loading={loading}
+                options={tasksListIds}
+                getOptionLabel={(taskId): string =>
+                  taskList.find((task) => task.id === taskId)?.label ?? ""
+                }
+                renderInput={(params): ReactElement => (
+                  <TextField
+                    {...params}
+                    variant="outlined"
+                    label={"Task List"}
+                    onChange={(e) => {
+                      handleTaskListSearch(e.target.value || "");
+                    }}
+                    InputProps={{
+                      ...params.InputProps,
+                      "aria-labelledby": "Task List",
+                      endAdornment: (
+                        <>
+                          {loading && (
+                            <CircularProgress color="primary" size={20} />
+                          )}
+                          {params.InputProps.endAdornment}
+                        </>
+                      ),
+                    }}
+                  />
+                )}
+                value={tasktField.value}
+                onChange={(_, taskId) => handleTaskListSelected(taskId)}
+                sx={{ mb: 2 }}
+              />
+              {taskMeta.error && (
+                <FormHelperText>{taskMeta.error}</FormHelperText>
+              )}
+            </FormControl>
+          )}
+        </Field>
       )}
-      value={projectField.value}
-      onChange={(_, donorAccountId) => projectField.onChange(donorAccountId)}
-      // isOptionEqualToValue={(option, value): boolean => option === value}
-    />
-    
-  )
-}
+    </>
+  );
+};
